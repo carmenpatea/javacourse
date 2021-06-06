a157c0fb31b54338267fd960a5dbdb599cb703c6
carmen.patea <carmen.patea@nagarro.com>
2021-06-07 01:02:50 +0300

    add exercice 5.1-> 5.3 ; 6.1 -> 6.3 ; 7.1; 8.1 -> 8.3

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -1,9 +1,63 @@
 package duke.choice;
 
-public class Clothing {
+import java.util.Arrays;
+
+public class Clothing implements Comparable<Clothing> {
     String description;
     double price;
     String size = "M";
 
+    public final static int MIN_PRICE = 10;
+    public final static double MIN_TAX = 0.2;
 
+    public Clothing(String description, double price, String newSize) {
+        this.description = description;
+        this.price = price;
+        size = newSize;
+    }
+
+    public Clothing(String description, String size) {
+        this.description = description;
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price + price*MIN_TAX;
+    }
+
+    public void setPrice(double price) {
+       /* if ( price >= MIN_PRICE) {
+            this.price = price;
+        }*/
+        this.price= (( price >= MIN_PRICE) ? price : MIN_PRICE );
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String toString() {
+        return "Clothing:" +
+                "description='" + description + '\'' +
+                ", size=" + size +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Clothing clothing) {
+        return this.description.compareTo(clothing.description);
+    }
 }
--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -1,47 +1,152 @@
 package duke.choice;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ShopApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Duke Choice Shop");
-		// ex3.1
-		Customer c1 = new Customer();
+		System.out.println("---------exercise 3.1:------");
+		Customer c1 = new Customer("PinkeC1");
 		c1.name = "Pinke";
 		System.out.println("Hello " + c1.name);
 
-		//ex 3.2
+		System.out.println("---------exercise 3.2:------");
 		double tax = 0.2, total = 0.0;
 
-		Clothing item1 = new Clothing();
-		Clothing item2 = new Clothing();
+		System.out.println("---------exercise 6.3:Static demo----------");
+		System.out.println("Min price is " + Clothing.MIN_PRICE);
 
-		//ex 4.2
-		Clothing[] items = {item1, item2};
+		System.out.println("TAX Rate is "  + Clothing.MIN_TAX);
+		System.out.println();
 
-		item1.description = "Blue Jacket";
+		Clothing item1 = new Clothing("Blue Jacket",20.9, "M");
+		Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
+
+		System.out.println("---------exercise 3.3:------");
+		Clothing[] items = {item1, item2, new Clothing("Black Jacket",27.9, "XL"), new Clothing("Red T-Shirt", 55.5, "XS")};
+
+		/*item1.description = "Blue Jacket";
 		item1.price = 20.9;
 		item1.size = "M";
 		item2.description = "Orange T-Shirt";
 		item2.price = 10.5;
-		item2.size = "S";
+		item2.size = "S";*/
 		System.out.println("item1 are " + item1.description + " " + item1.size + " " + item1.price);
 
 		total = item1.price + item1.price * tax + 2 * item2.price + 2 * item2.price * tax;
 
 		System.out.println("Total to pay " + total);
 
-		//4.1
+		System.out.println("---------exercise 4.1:------");
 		int measurement = 3;
-		Customer c2 = new Customer();
+		Customer c2 = new Customer("c2");
 		c2.size = "S";
 
 		switch (measurement) {
-			case 1: case 2: case 3: c2.size = "S"; break;
-			case 4: case 5: case 6: c2.size = "M"; break;
-			case 7: case 8: case 9: c2.size = "L"; break;
-			default: c2.size = "X";
+			case 1:
+			case 2:
+			case 3:
+				c2.size = "S";
+				break;
+			case 4:
+			case 5:
+			case 6:
+				c2.size = "M";
+				break;
+			case 7:
+			case 8:
+			case 9:
+				c2.size = "L";
+				break;
+			default:
+				c2.size = "X";
 		}
 		System.out.println(c2.size);
+
+		System.out.println("---------exercise 4.3:------");
+		for ( Clothing item: items) {
+			if(c2.getSize().equals(item.getSize())) {
+				total = total + item.getPrice();
+				System.out.println("Item " + "," + item.getDescription() + ","+ item.getPrice()+"'" + item.getSize());
+				if(total >15) { break;}
+			}
+		}
+
+
+		System.out.println("-------exercise 5.1: Apply encapsulation---------");
+		Clothing item3 = new Clothing("Blue Jacket",9,"M");
+		item3.setPrice(9.0);
+		System.out.println("Price is 9 with tax  " + item3.getPrice());
+		item3.setPrice(10.0);
+		System.out.println("Price is 10 wih tax " + item3.getPrice());
+		item3.setPrice(11.0);
+		System.out.println("Price is 11 with tax  " + item3.getPrice());
+
+		//???????
+		for ( Clothing item: items) {
+			if(c2.getSize().equals(item.getSize())) {
+				total = total + total*tax;
+				System.out.println("Total price with tax:  " + total);
+				if(total >15) { break;}
+			}
+		}
+
+		System.out.println("---------exercise 5.2:Oveload a method----------");
+		int measurementNew = 5;
+		Customer c5 = new Customer("Lucy");
+		c5.setName("Lucy");
+		c5.setSize("S");
+		System.out.println("Customer5 " + c5.getName() + " has size: " + c5.getSize());
+
+		c5.setSize(measurementNew);
+		System.out.println("Now Customer5 " + c5.getName() + " has size: " + c5.getSize());
+
+		System.out.println("---------exercise 5.3:Oveload a method----------");
+
+		items[2].setDescription("Black Jacket");
+		items[3].setDescription("RED T-Shirt");
+		c5.addItems(items);
+		c5.setSize(2);
+		System.out.println("Customer5 " + c5.getName() + " has totla cost for his clothing: " + c5.getTotalClothingCost());
+		for ( Clothing item: c5.getItems()) {
+			System.out.println("Clothings " + item.getDescription());
+		}
+
+		System.out.println("---------exercise 6.1: Object demo----------");
+
+		Customer newCustomer = new Customer("Pinky",3);
+		System.out.println("Now Customer5 " + newCustomer.getName() + " has size: " + newCustomer.getSize());
+		System.out.println();
+
+		System.out.println("---------exercise 6.2:Constructor demo----------");
+		Clothing item5 = new Clothing("Blue Jacket",20.9, "M");
+		System.out.println("New constructor for Item5 " + ": " + item5.getDescription() + ", "+ item5.getPrice()+", " + item5.getSize());
+		System.out.println();
+
+		System.out.println("---------exercise 6.3:Static demo----------");
+		System.out.println("See top of page: Min price , before declare class Clothing");
+		System.out.println("See top of page: TAX Rate ");
+		System.out.println();
+
+		System.out.println("---------exercise 7.1: Handling exceptions----------");
+		c5.averagePrice(items);
+		System.out.println();
+
+		System.out.println("---------exercise 8.1:Abstarct class----------");
+		for ( Clothing item: c5.getItems()) {
+			System.out.println("Clothings " + item.toString());
+		}
+		System.out.println();
+
+		System.out.println("---------exercise 8.2:Interface----------");
+		Arrays.sort(c5.getItems());
+		for ( Clothing item: c5.getItems()) {
+			System.out.println("Item output " + item);
+		}
+		System.out.println();
+
 
 	}
 }
